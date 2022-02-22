2fd598d2b72b5ce5ed685339918168c6a14f78e9
Junho <90366348+JunhoBaee@users.noreply.github.com>
2022-02-22 12:57:37 +0900

    js

---
+++ javascript_교재실습/js/selector.js
@@ -0,0 +1,19 @@
+const frame = document.querySelector("#wrap .box1")
+console.log(frame)
+const item = document.querySelector("#wrap article")
+console.log(item)
+const items = document.querySelectorAll("#wrap")
+console.log(items)
+console.log(items[0])
+
+const pi = 3.14; // const 선언을 했으면 그 다음에 값의 변경이 불가함
+console.log(pi)
+// pi = 6;
+
+console.log(pi)
+
+let bbb = "안녕하세요"; // 추 후 값의 변경이 가능함
+console.log(bbb, typeof(bbb))
+
+ccc=true;
+console.log(ccc, typeof(ccc)) // ccc에 값이 들어가 있어야 함
