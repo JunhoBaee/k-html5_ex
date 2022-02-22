3698eba6b8b8e61af1ba947ee1b2f6b6fbbc2c05
Junho <90366348+JunhoBaee@users.noreply.github.com>
2022-02-22 17:52:04 +0900

    .

--- javascript_교재실습/js/selector.js
+++ javascript_교재실습/js/selector.js
@@ -4,16 +4,55 @@ const item = document.querySelector("#wrap article")
 console.log(item)
 const items = document.querySelectorAll("#wrap")
 console.log(items)
-console.log(items[0])
 
-const pi = 3.14; // const 선언을 했으면 그 다음에 값의 변경이 불가함
-console.log(pi)
-// pi = 6;
 
-console.log(pi)
+let num = 40
+console.log("나의 나이는 $num 입니다.")
+console.log("나의 나이는 ${num} 입니다.")
 
-let bbb = "안녕하세요"; // 추 후 값의 변경이 가능함
-console.log(bbb, typeof(bbb))
 
-ccc=true;
-console.log(ccc, typeof(ccc)) // ccc에 값이 들어가 있어야 함
+
+// console.log(items.length)
+// for(let i = 0; i <= items.length; i++){
+//     console.log(items[i]);
+// }
+
+
+// const pi = 3.14; // const 선언을 했으면 그 다음에 값의 변경이 불가함
+// console.log(pi)
+// // pi = 6;
+
+// console.log(pi)
+
+// let bbb = "안녕하세요"; // 추 후 값의 변경이 가능함
+// console.log(bbb, typeof(bbb))
+
+// ccc=true;
+// console.log(ccc, typeof(ccc)) // ccc에 값이 들어가 있어야 함
+
+// console.log("PPT 실습시작");
+// // ppt 실습
+// const isTrue = true;
+// const isFalse = false;
+
+// console.log(isTrue, typeof isTrue);
+// console.log(isFalse, typeof isFalse);
+
+// const a = new Boolean(false);
+// console.log(a, typeof a); //
+
+// const b = null;
+// console.log(b, typeof b); 
+// // * object => null도 하나의 오브젝트로 인식해서 null로 출력
+// // 값을 비교할 때는 null인지 비교하기 떄문에 typeof는 null이 어떤 타입으로 인식하는지 이해하면됨
+
+// console.log(a == b); // true false
+// console.log(a === b); // 둘아 아예 일치하는 지 비교
+
+// //undefined
+// let c;
+// console.log(c, typeof c); // 단순히 객체만 불러옴
+
+// c = undefined;
+// console.log(c, typeof c); // 객체인 c에 저장되어 있는 undefined라는 값을 불러옴
+
