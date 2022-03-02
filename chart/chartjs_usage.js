4a7534c9b7a1eb2017705203ea1610ebadcbbb78
junho <zk836566@gmail.com>
2022-03-02 15:31:12 +0900

    .

---
+++ chart/chartjs_usage.js
@@ -0,0 +1,5 @@
+// Any of the following formats may be used
+const ctx = document.getElementById('myChart');
+const ctx = document.getElementById('myChart').getContext('2d');
+const ctx = $('#myChart');
+const ctx = 'myChart';
